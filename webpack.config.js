e6a9873b370a1e6110d1890fa2abce46e85f251b
Bart Wijnants <bartwijnants@outlook.com>
2017-07-09 12:06:03 +0200

    optimize sourcemaps and prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  devtool: "source-map",
+  devtool: "cheap-module-eval-source-map",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -15,13 +15,5 @@ module.exports = {
       { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
       { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+  }
 };
