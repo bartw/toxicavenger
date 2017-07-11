9159a514d1278b4660c100d786a2fbe02f319783
Bart Wijnants <bartwijnants@outlook.com>
2017-07-11 21:07:40 +0200

    add member functions

--- src/services/TeamService.js
+++ src/services/TeamService.js
@@ -3,9 +3,9 @@ import _ from "lodash";
 
 function mapTeams(snapshot) {
   return _(snapshot.val())
-    .mapValues((teams, uid) =>
+    .mapValues((teams, owner) =>
       _(teams)
-        .mapValues((team, id) => _.merge({}, team, { id }, { uid }))
+        .mapValues((team, id) => _.merge({}, team, { id }, { owner }))
         .values()
         .value()
     )
@@ -20,22 +20,47 @@ export default class TeamService {
       onChanged(mapTeams(snapshot));
     });
 
-    this.add = (uid, name) => {
-      if (!uid || !name) {
+    this.add = (owner, name) => {
+      if (!owner || !name) {
         return;
       }
       firebase
         .database()
-        .ref("teams/" + uid)
-        .push({ name: name, members: [uid] });
+        .ref("teams/" + owner)
+        .push({ name: name, members: [owner] });
     };
 
-    this.join = (uid, id) => {
-      firebase.database().ref("teamrequests/" + id + "/" + uid).set(true);
+    this.join = (member, id) => {
+      firebase.database().ref("teamrequests/" + id + "/" + member).set(true);
     };
 
-    this.delete = (uid, id) => {
-      firebase.database().ref("teams/" + uid + "/" + id).remove();
+    this.approve = (owner, member, id) => {
+      const ref = firebase.database().ref("teams/" + owner + "/" + id);
+      ref.once("value").then(snapshot => {
+        const members = snapshot.val().members;
+        firebase.database().ref("teams/" + owner + "/" + id).set({
+          members: [...members, member]
+        });
+        firebase.database().ref("teamrequests/" + id + "/" + member).remove();
+      });
+    };
+
+    this.decline = (member, id) => {
+      firebase.database().ref("teamrequests/" + id + "/" + member).remove();
+    };
+
+    this.removeMember = (owner, member, id) => {
+      const ref = firebase.database().ref("teams/" + owner + "/" + id);
+      ref.once("value").then(snapshot => {
+        const members = snapshot.val().members;
+        firebase.database().ref("teams/" + owner + "/" + id).set({
+          members: members.filter(existingMember => existingMember !== member)
+        });
+      });
+    };
+
+    this.delete = (owner, id) => {
+      firebase.database().ref("teams/" + owner + "/" + id).remove();
     };
   }
 }
