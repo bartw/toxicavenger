0054bb9f7a9ff129e0dd22acef10bd850507584f
Bart Wijnants <bartwijnants@outlook.com>
2017-08-01 12:27:47 +0200

    dev deploy

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
+const firebaseConfig = process.env.TRAVIS_TAG
+  ? {
+      apiKey: "AIzaSyDGL-so34ZsCcf_TOsRIfbzfmUy0QXzb9g",
+      authDomain: "toxicavenger-a5a9a.firebaseapp.com",
+      databaseURL: "https://toxicavenger-a5a9a.firebaseio.com",
+      projectId: "toxicavenger-a5a9a"
+    }
+  : null;
 
 module.exports = {
   entry: "./src/index.js",
@@ -13,12 +21,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      FIREBASE_CONFIG: JSON.stringify({
-        apiKey: "AIzaSyDGL-so34ZsCcf_TOsRIfbzfmUy0QXzb9g",
-        authDomain: "toxicavenger-a5a9a.firebaseapp.com",
-        databaseURL: "https://toxicavenger-a5a9a.firebaseio.com",
-        projectId: "toxicavenger-a5a9a"
-      })
+      FIREBASE_CONFIG: JSON.stringify(firebaseConfig)
     })
   ]
 };
